769c44fcc2979b562480b059d8a87cea8df33aab
Wei Tang <tangwei1027@gmail.com>
2019-07-18 11:40:07 -0500

    debugging auto cutter

--- auto_cut_help_fun.py
+++ auto_cut_help_fun.py
@@ -1,7 +1,175 @@
 from qiskit import QuantumCircuit
 from qiskit.dagcircuit.dagcircuit import DAGCircuit
 from qiskit.converters import circuit_to_dag, dag_to_circuit
-import min_cut as cut_finder
+import random
+import math
+import copy
+from collections import Counter
+from datetime import datetime
+import sys
+
+class Graph(object):
+    def __init__(self, vlist):
+        self.verts = {v[0]:Counter(v[1:]) for v in vlist}
+        self.update_edges()
+
+    def update_edges(self):
+        self.edges = []
+        
+        for k, v in self.verts.items():
+            self.edges += ([(k, t) for t in v.keys() for n in range(v[t]) if k < t])
+
+    @property
+    def vertex_count(self):
+        return len(self.verts)
+
+    @property
+    def edge_count(self):
+        return len(self.edges)
+
+    def merge_vertices(self, edge_index):
+        head_key, tail_key = self.edges[edge_index]
+        # print('removing edge ', self.edges[edge_index], head_key, tail_key)
+        
+        head = self.verts[head_key]
+        tail = self.verts[tail_key]
+
+        # print('merging head =', head_key, head, 'tail =', tail_key, tail)
+
+        # Remove the edge between head and tail
+        del head[tail_key]
+        del tail[head_key]
+
+        # print('After removing head->tail, head =', head, 'tail =', tail)
+
+        # Merge tails
+        head.update(tail)
+
+        # print('After merging tail, head =', head_key, head)
+
+        # Update all the neighboring vertices of the fused vertex
+        for i in tail.keys():
+            v = self.verts[i]
+            # print('tail neighboring vertex', i, 'vertex', v)
+            v[head_key] += v[tail_key]
+            del v[tail_key]
+            # print('updated to', v)
+            
+        # Finally remove the tail vertex
+        del self.verts[tail_key]
+
+        self.update_edges()
+        # print('FINISHED edges:', self.edges)
+        # print('FINISHED verts:', self.verts)
+
+def translate(original_r, original_g, curr_g, grouping):
+    original_head, original_tail = original_g.edges[original_r]
+    curr_head = None
+    if original_head in curr_g.verts:
+        curr_head = original_head
+    else:
+        for group in grouping:
+            if original_head in group.split(' '):
+                curr_head = group.split(' ')[0]
+    curr_tail = None
+    if original_tail in curr_g.verts:
+        curr_tail = original_tail
+    else:
+        for group in grouping:
+            if original_tail in group.split(' '):
+                curr_tail = group.split(' ')[0]
+    curr_r = -1
+    for edge_idx, edge in enumerate(curr_g.edges):
+        if (curr_head, curr_tail) == edge:
+            curr_r = edge_idx
+    return curr_r
+
+def contract(graph, min_v=2):
+    g = copy.deepcopy(graph)
+    grouping = [x for x in g.verts]
+    contracted_edges = []
+    contraction_order = random.sample(range(0,graph.edge_count), graph.edge_count)
+    i = 0
+    # print('initial grouping:', grouping)
+    while g.vertex_count > min_v:
+        # r = random.randrange(0, g.edge_count)
+        # head, tail = g.edges[r]
+        # graph_r = random.randrange(0, graph.edge_count)
+        graph_r = contraction_order[i]
+        i+=1
+        contracted_edges.append(graph.edges[graph_r])
+        g_r = translate(original_r=graph_r, original_g=graph, curr_g=g, grouping=grouping)
+        g_head, g_tail = g.edges[g_r]
+        print('contracting', graph.edges[graph_r])
+
+        # print('keep the edge', (head, tail))
+        g.merge_vertices(g_r)
+        
+        hi = -1
+        ti = -1
+        for group_idx, group in enumerate(grouping):
+            if g_head in group:
+                hi = group_idx
+            if g_tail in group:
+                ti = group_idx
+        grouping.append(grouping[hi] + ' ' + grouping[ti])
+        del grouping[min(hi,ti)]
+        del grouping[max(hi,ti)-1]
+        # print('updated grouping:', grouping)
+
+    cut_edges = []
+    for edge in graph.edges:
+        if edge not in contracted_edges:
+            cut_edges.append(edge)
+            print('original edge', edge)
+            print('is not contracted in', contracted_edges)
+            print()
+    return g, grouping, cut_edges
+
+def cluster_character(grouping):
+    # FIXME: double check d calculation
+    max_d = 0
+    for group in grouping:
+        d = 0
+        for vertex in group.split(' '):
+            qargs = vertex.split(',')
+            for qarg in qargs:
+                if qarg[len(qarg)-1] == '0':
+                    d += 1
+        max_d = max(max_d, d)
+    return max_d
+
+# Karger's Algorithm
+# For failure probabilty upper bound of 1/n, repeat the algorithm nC2 logn times
+def min_cut(graph):
+    m = graph.edge_count
+    n = graph.vertex_count
+    all_K_d = {}
+    print('will run %d times' % int(n * (n-1) * math.log(n)/2))
+    for i in range(int(n * (n-1) * math.log(n)/2)):
+        random.seed(datetime.now())
+        g, grouping, cut_edges = contract(graph)
+        m = min(m, g.edge_count)
+        K = g.edge_count
+        d = cluster_character(grouping)
+        # TODO: K,d cluster is overwritten
+        all_K_d[(K,d)] = cut_edges
+        # print('Run %d cut_edges:' % i, cut_edges)
+        # print('*'* 100)
+    
+    pareto_K_d = {}
+    for key0 in all_K_d:
+        K0, d0 = key0
+        is_pareto = True
+        for key1 in all_K_d:
+            K1, d1 = key1
+            if key1!=key0 and ((K1<K0 and d1<d0) or (K1==K0 and d1<d0) or (K1<K0 and d1==d0)):
+                is_pareto = False
+                break
+        if is_pareto:
+            pareto_K_d[key0] = all_K_d[key0]
+
+    return pareto_K_d
 
 def find_neighbor(qarg, vert_info):
     for idx, (vertex_name, qargs) in enumerate(vert_info):
@@ -47,5 +215,11 @@ def circuit_to_graph(stripped_circ):
             if next_found:
                 abstraction_item.append(next_vertex_name)
         abstraction.append(abstraction_item)
-    graph = cut_finder.Graph(abstraction) 
+    graph = Graph(abstraction) 
     return graph
+
+def find_pareto_solutions(circ):
+    stripped_circ = circ_stripping(circ)
+    graph = circuit_to_graph(stripped_circ)
+    pareto_K_d = min_cut(graph)
+    return pareto_K_d
--- min_cut.py
+++ min_cut.py
@@ -29,30 +29,23 @@ class Graph(object):
 
     def merge_vertices(self, edge_index):
         hi, ti = self.edges[edge_index]
-        # print('removing edge ', self.edges[edge_index])
         
         head = self.verts[hi]
         tail = self.verts[ti]
-
-        # print('head = ', head, 'tail = ', tail)
 
         # Remove the edge between head and tail
         del head[ti]
         del tail[hi]
 
-        # print('After removing head->tail, head = ', head, 'tail = ', tail)
-
         # Merge tails
         head.update(tail)
-
-        # print('After merging tail, head = ', head)
 
         # Update all the neighboring vertices of the fused vertex
         for i in tail.keys():
             v = self.verts[i]
             v[hi] += v[ti]
             del v[ti]
-            
+
         # Finally remove the tail vertex
         del self.verts[ti]
 
@@ -78,6 +71,7 @@ def min_cut(graph):
         # print(i, m)
     return m
 
+
 def _fast_min_cut(graph):
     if graph.vertex_count <= 6:
         return min_cut(graph)
@@ -86,7 +80,6 @@ def _fast_min_cut(graph):
         g1 = contract(graph, t)
         g2 = contract(graph, t)
         
-        # print('m = ', min(_fast_min_cut(g1), _fast_min_cut(g2)))
         return min(_fast_min_cut(g1), _fast_min_cut(g2))
 
 # Karger Stein algorithm. Refer https://en.wikipedia.org/wiki/Karger%27s_algorithm#Karger.E2.80.93Stein_algorithm
@@ -94,7 +87,6 @@ def _fast_min_cut(graph):
 def fast_min_cut(graph):
     m = graph.edge_count
     n = graph.vertex_count
-    print('will run %d times' % int(n * math.log(n)/(n - 1)))
     for i in range(int(n * math.log(n)/(n - 1))):
         random.seed(datetime.now())
         m = min(m, _fast_min_cut(graph))
@@ -102,10 +94,5 @@ def fast_min_cut(graph):
     return m
     
 # Simple test
-# graph = Graph([[4,6,7], [5,6,7], [6,4,5], [7,4,5]])
-# graph = Graph([['a','c','d'],['b','c','d'],['c','a','b'],['d','a','b']])
-graph = Graph([['a','c','d'],['b','c','d'],['c','a','b'],['d','a','b']])
-print(graph.verts)
-print(graph.edges)
-print('*' * 100)
+graph = Graph([[1,2,3],[2,1,3,4],[3,1,2,4],[4,2,3]])
 print(fast_min_cut(graph))
