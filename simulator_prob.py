68934fc9eb1fec08d493573d1cd2bf7b872e4ff9
weiT1993 <tangwei1027@gmail.com>
2019-09-23 16:38:39 -0400

    working on prob simulator

--- simulator_prob.py
+++ simulator_prob.py
@@ -6,6 +6,7 @@ import glob
 import itertools
 import copy
 import os
+import numpy as np
 
 def reverseBits(num,bitSize): 
     binary = bin(num)
@@ -13,8 +14,8 @@ def reverseBits(num,bitSize):
     reverse = reverse + (bitSize - len(reverse))*'0'
     return int(reverse,2)
 
-def simulate_circ(circ, simulator='statevector_simulator'):
-    backend = BasicAer.get_backend(simulator)
+def simulate_circ(circ, simulator):
+    backend = BasicAer.get_backend('statevector_simulator')
     job = execute(circ, backend=backend)
     result = job.result()
     outputstate = result.get_statevector(circ)
@@ -22,26 +23,56 @@ def simulate_circ(circ, simulator='statevector_simulator'):
     for i, sv in enumerate(outputstate):
         reverse_i = reverseBits(i,len(circ.qubits))
         outputstate_ordered[reverse_i] = sv
-    return outputstate_ordered
+    if simulator == 'sv':
+        return outputstate_ordered
+    elif simulator == 'prob':
+        output_prob = [np.power(np.absolute(x),2) for x in outputstate_ordered]
+        return output_prob
+    else:
+        raise Exception('Illegal simulator')
 
-def find_cluster_cut_qubit(complete_path_map,cluster_idx):
-    ancilla_indices = []
+def find_cluster_O_rho_qubits(complete_path_map,cluster_idx):
+    O_qubits = []
+    rho_qubits = []
     for input_qubit in complete_path_map:
         path = complete_path_map[input_qubit]
         if len(path)>1:
-            for cut_qubit in path[1::]:
-                if cut_qubit[0] == cluster_idx:
-                    ancilla_indices.append(cut_qubit[1])
-    return ancilla_indices
+            for q in path[:-1]:
+                if q[0] == cluster_idx:
+                    O_qubits.append(q)
+            for q in path[1:]:
+                if q[0] == cluster_idx:
+                    rho_qubits.append(q)
+    return O_qubits, rho_qubits
 
-def init_key(init,ancilla_indices,num_qubits):
-    key = [0 for i in range(num_qubits)]
-    for i, idx in enumerate(ancilla_indices):
-        key[idx] = init[i]
-    key = tuple(key)
-    return key
+def find_all_simulation_combinations(O_qubits, rho_qubits, num_qubits):
+    print('Rho qubits:',rho_qubits)
+    all_inits = list(itertools.product(init_states,repeat=len(rho_qubits)))
+    complete_inits = []
+    for init in all_inits:
+        complete_init = ['zero' for i in range(num_qubits)]
+        for i in range(len(init)):
+            complete_init[rho_qubits[i][1]] = init[i]
+        complete_inits.append(complete_init)
+    print('initializations:',complete_inits)
+
+    print('O qubits:',O_qubits)
+    all_meas = list(itertools.product(measurement_basis,repeat=len(O_qubits)))
+    complete_meas = []
+    for meas in all_meas:
+        complete_m = ['I' for i in range(num_qubits)]
+        for i in range(len(meas)):
+            complete_m[O_qubits[i][1]] = meas[i]
+        complete_meas.append(complete_m)
+    print('measurement basis:',complete_meas)
+
+    combinations = list(itertools.product(complete_inits,complete_meas))
+    print(len(combinations))
+
 
 if __name__ == '__main__':
+    measurement_basis = ['I','X','Y']
+    init_states = ['zero','one','plus','minus','plus_i','minus_i']
     dirname = './data'
     complete_path_map = pickle.load(open( '%s/cpm.p'%dirname, 'rb' ))
 
@@ -49,25 +80,10 @@ if __name__ == '__main__':
 
     cluster_circ_files = [f for f in glob.glob(dirname+'/cluster_*_circ.p')]
     for cluster_idx in range(len(cluster_circ_files)):
-        cluster_sv = {}
-        ancilla_indices = find_cluster_cut_qubit(complete_path_map,cluster_idx)
-        all_inits = list(itertools.product(range(0,2),repeat=len(ancilla_indices)))
-        print('cluster %d, ancillas:'%cluster_idx,ancilla_indices)
-
-        for init in all_inits:
-            print('init conditions:',init)
-            cluster_circ = pickle.load(open( ('%s/cluster_%d_circ.p'%(dirname,cluster_idx)), 'rb' ))
-            cluster_dag = circuit_to_dag(cluster_circ)
-            for idx in range(len(ancilla_indices)):
-                ancilla_qubit = cluster_circ.qubits[ancilla_indices[idx]]
-                init_cond = init[idx]
-                if init_cond == 1:
-                    cluster_dag.apply_operation_front(op=XGate(),qargs=[ancilla_qubit],cargs=[])
-            cluster_circ = dag_to_circuit(cluster_dag)
-            print(cluster_circ)
-            sv = simulate_circ(cluster_circ)
-            init = init_key(init,ancilla_indices,len(cluster_circ.qubits))
-            print('saved as:',init)
-            cluster_sv[init] = sv
-        pickle.dump(cluster_sv, open('%s/cluster_%d_sv.p'%(dirname,cluster_idx), 'wb' ))
+        print('cluster %d'%cluster_idx)
+        cluster_prob = {}
+        cluster_circ = pickle.load(open(('%s/cluster_%d_circ.p'%(dirname,cluster_idx)), 'rb' ))
+        print(cluster_circ)
+        O_qubits, rho_qubits = find_cluster_O_rho_qubits(complete_path_map,cluster_idx)
+        find_all_simulation_combinations(O_qubits, rho_qubits, len(cluster_circ.qubits))
         print('-'*100)
