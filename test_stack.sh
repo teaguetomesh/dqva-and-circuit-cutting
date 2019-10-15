3dcc4aadb7ba097828ec42c57281c2618be9793a
weiT1993 <tangwei1027@gmail.com>
2019-10-14 22:49:47 -0400

    debugged plotter

--- test_stack.sh
+++ test_stack.sh
@@ -1,17 +1,20 @@
 python generate_evaluator_input.py
 
-EVALUATOR_FILES=./noisy_benchmark_data/evaluator_input_*.p
-for f in $EVALUATOR_FILES;
+for i in {1..2};
 do
-    echo $f
-    mpiexec -n 3 python evaluator_prob.py --input-file $f --shots 10000
-done
+    EVALUATOR_FILES=./noisy_benchmark_data/evaluator_input_*.p
+    for f in $EVALUATOR_FILES;
+    do
+        echo $f
+        mpiexec -n 3 python evaluator_prob.py --input-file $f --shots 1000
+    done
 
-UNITER_FILES=./noisy_benchmark_data/uniter_input_*.p
-for f in $UNITER_FILES;
-do
-    echo $f
-    python uniter_prob.py --input-file $f
-done
+    UNITER_FILES=./noisy_benchmark_data/uniter_input_*.p
+    for f in $UNITER_FILES;
+    do
+        echo $f
+        python uniter_prob.py --input-file $f
+    done
 
-python data_combiner.py
+    python data_combiner.py
+done
