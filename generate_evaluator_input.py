7b4d96ccd188f78417eb88cf4a9330fd690a52b4
weiT1993 <tangwei1027@gmail.com>
2019-11-09 12:10:03 -0500

    updated plotter

--- generate_evaluator_input.py
+++ generate_evaluator_input.py
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 
     # NOTE: toggle circuits to benchmark
     # dimension_l = [[2,2],[2,3],[3,3],[2,5],[3,4],[4,4]]
-    dimension_l = [[2,5],[3,4],[4,4]]
+    dimension_l = [[4,4]]
     dirname = './benchmark_data'
     if not os.path.exists(dirname):
         os.mkdir(dirname)
--- test_stack.sh
+++ test_stack.sh
@@ -1,6 +1,6 @@
 # NOTE: toggle here to change max qc size, max clusters
 echo "Generate evaluator input"
-python generate_evaluator_input.py --min-qubit 3 --max-qubit 9 --max-clusters 5 --device-name ibmq_boeblingen
+python generate_evaluator_input.py --min-qubit 9 --max-qubit 9 --max-clusters 5 --device-name ibmq_boeblingen
 EVALUATOR_FILE=./benchmark_data/evaluator_input_*.p
 
 for i in {1..1};
