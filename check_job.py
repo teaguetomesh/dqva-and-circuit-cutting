d5181c72c1f72180d3aa610ce4b3c7a8817f6b59
weiT1993 <tangwei1027@gmail.com>
2019-11-12 15:50:01 -0500

    minor changes to plotter

--- check_job.py
+++ check_job.py
@@ -4,6 +4,7 @@ from qiskit.providers.aer import noise
 from qiskit import IBMQ
 from qiskit.providers.jobstatus import JobStatus
 import argparse
+from qiskit.visualization import plot_gate_map, plot_error_map
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='check hardware jobs')
@@ -17,6 +18,7 @@ if __name__ == '__main__':
             evaluator_info = get_evaluator_info(circ=None,device_name=str(x),fields=['properties'])
             num_qubits = len(evaluator_info['properties'].qubits)
             print('%s: %d-qubit, max %d jobs * %d shots'%(x,num_qubits,x.configuration().max_experiments,x.configuration().max_shots))
+            # plot_error_map(x).savefig('./plots/%s_error_map.png'%str(x))
             if str(x) == 'ibmq_boeblingen':
                 for job in x.jobs():
                     if job.status() != JobStatus['RUNNING']:
