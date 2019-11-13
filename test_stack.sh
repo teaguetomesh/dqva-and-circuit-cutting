a14b5d852a0867284f36759a693a2d1d587d3412
weiT1993 <tangwei1027@gmail.com>
2019-11-13 02:06:25 -0500

    updated benchmark

--- test_stack.sh
+++ test_stack.sh
@@ -6,7 +6,7 @@ rm -r plots
 mkdir plots
 # NOTE: toggle here to change max qc size, max clusters
 echo "Generate evaluator input"
-python generate_evaluator_input.py --min-qubit 3 --max-qubit 9 --max-clusters 3 --device-name ibmq_boeblingen 2>&1 | tee ./logs/generator_logs.txt
+python generate_evaluator_input.py --min-qubit 3 --max-qubit 9 --max-clusters 4 --device-name ibmq_boeblingen 2>&1 | tee ./logs/generator_logs.txt
 
 echo "Running evaluator"
 # mpiexec -n 5 python evaluator_prob.py --saturated-shots --evaluation-method statevector_simulator --device-name ibmq_boeblingen
