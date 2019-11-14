a16f09b5d29a93bdd6217cb51348de5d95397ab6
weiT1993 <tangwei1027@gmail.com>
2019-11-14 18:36:01 -0500

    minor debug

--- test_stack.sh
+++ test_stack.sh
@@ -15,7 +15,7 @@ python generate_evaluator_input.py --min-qubit 2 --max-qubit 8 --max-clusters 4 
 echo "Running evaluator"
 mpiexec -n 2 python evaluator_prob.py --evaluation-method hardware --device-name ibmq_johannesburg --circuit-name supremacy
 echo "Running job submittor"
-python hardware_job_submittor.py --device-name ibmq_johannesburg 2>&1 | tee ./logs/hw_job_submittor_logs.txt
+python hardware_job_submittor.py --device-name ibmq_johannesburg --circuit-name supremacy 2>&1 | tee ./logs/hw_job_submittor_logs.txt
 echo "Running reconstruction"
 python uniter_prob.py --evaluation-method hardware --device-name ibmq_johannesburg 2>&1 | tee ./logs/uniter_logs.txt
 
