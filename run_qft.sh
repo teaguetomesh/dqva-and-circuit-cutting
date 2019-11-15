674bc841b0db1b219763510dbc0744b425691c8c
weiT1993 <tangwei1027@gmail.com>
2019-11-15 14:27:57 -0500

    added qft benchmarks

--- run_qft.sh
+++ run_qft.sh
@@ -1,22 +1,22 @@
 # NOTE: toggle here to change max qc size, max clusters
 echo "Generate evaluator input"
-python generate_evaluator_input.py --min-qubit 2 --max-qubit 8 --max-clusters 4 --device-name ibmq_20_tokyo --circuit-name qft 2>&1 | tee ./logs/qft_generator_logs.txt
+python generate_evaluator_input.py --min-qubit 2 --max-qubit 8 --max-clusters 4 --device-name ibmq_poughkeepsie --circuit-name qft 2>&1 | tee ./logs/qft_generator_logs.txt
 
 echo "Running evaluator"
-mpiexec -n 2 python evaluator_prob.py --device-name ibmq_20_tokyo --circuit-name qft --evaluation-method hardware
+mpiexec -n 2 python evaluator_prob.py --device-name ibmq_poughkeepsie --circuit-name qft --evaluation-method hardware
 echo "Running job submittor"
-python hardware_job_submittor.py --device-name ibmq_20_tokyo --circuit-name qft 2>&1 | tee ./logs/qft_hw_job_submittor_logs.txt
+python hardware_job_submittor.py --device-name ibmq_poughkeepsie --circuit-name qft 2>&1 | tee ./logs/qft_hw_job_submittor_logs.txt
 echo "Running reconstruction"
-python uniter_prob.py --device-name ibmq_20_tokyo --evaluation-method hardware --circuit-name qft 2>&1 | tee ./logs/qft_uniter_logs.txt
+python uniter_prob.py --device-name ibmq_poughkeepsie --evaluation-method hardware --circuit-name qft 2>&1 | tee ./logs/qft_uniter_logs.txt
 
 # python plot.py
 
 # echo "Running evaluator"
-# mpiexec -n 5 python evaluator_prob.py --evaluation-method statevector_simulator --device-name ibmq_20_tokyo --circuit-name qft
+# mpiexec -n 5 python evaluator_prob.py --evaluation-method statevector_simulator --device-name ibmq_poughkeepsie --circuit-name qft
 # echo "Running reconstruction"
-# python uniter_prob.py --evaluation-method statevector_simulator --device-name ibmq_20_tokyo 2>&1 | tee ./logs/uniter_logs.txt
+# python uniter_prob.py --evaluation-method statevector_simulator --device-name ibmq_poughkeepsie 2>&1 | tee ./logs/uniter_logs.txt
 
 # echo "Running evaluator"
-# mpiexec -n 5 python evaluator_prob.py --evaluation-method noisy_qasm_simulator --device-name ibmq_20_tokyo --circuit-name qft
+# mpiexec -n 5 python evaluator_prob.py --evaluation-method noisy_qasm_simulator --device-name ibmq_poughkeepsie --circuit-name qft
 # echo "Running reconstruction"
-# python uniter_prob.py --evaluation-method noisy_qasm_simulator --device-name ibmq_20_tokyo --circuit-name qft 2>&1 | tee ./logs/uniter_logs.txt
+# python uniter_prob.py --evaluation-method noisy_qasm_simulator --device-name ibmq_poughkeepsie --circuit-name qft 2>&1 | tee ./logs/uniter_logs.txt
