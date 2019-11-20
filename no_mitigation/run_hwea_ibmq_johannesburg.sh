f959d562756d09f99e3f33ad9456ac5c58d648b7
weiT1993 <tangwei1027@gmail.com>
2019-11-20 02:36:12 -0500

    added MPI generate_evaluator_input.py

---
+++ no_mitigation/run_hwea_ibmq_johannesburg.sh
@@ -0,0 +1,43 @@
+# NOTE: toggle here to change max qc size, max clusters
+echo "Generate evaluator input"
+python generate_evaluator_input.py --min-qubit 2 --max-qubit 9 --max-clusters 3 --device-name ibmq_johannesburg --circuit-type hwea 2>&1 | tee ./logs/hwea_ibmq_johannesburg_generator_logs.txt
+
+{
+echo "Running saturated hardware evaluator"
+mpiexec -n 2 python evaluator_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode saturated --evaluation-method hardware
+echo "Running job submittor"
+python hardware_job_submittor.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode saturated 2>&1 | tee ./logs/hwea_saturated_ibmq_johannesburg_hw_job_submittor_logs.txt
+echo "Running reconstruction"
+python uniter_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode saturated --evaluation-method hardware 2>&1 | tee ./logs/hwea_saturated_ibmq_johannesburg_uniter_logs.txt
+} &
+P1=$!
+
+{
+echo "Running sametotal hardware evaluator"
+mpiexec -n 2 python evaluator_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode sametotal --evaluation-method hardware
+echo "Running job submittor"
+python hardware_job_submittor.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode sametotal 2>&1 | tee ./logs/hwea_sametotal_ibmq_johannesburg_hw_job_submittor_logs.txt
+echo "Running reconstruction"
+python uniter_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode sametotal --evaluation-method hardware 2>&1 | tee ./logs/hwea_sametotal_ibmq_johannesburg_uniter_logs.txt
+} &
+P2=$!
+
+wait $P1 $P2
+echo "hwea ibmq_johannesburg DONE"
+
+# echo "Running saturated noisy qasm evaluator"
+# mpiexec -n 5 python evaluator_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode saturated --evaluation-method noisy_qasm_simulator
+# echo "Running reconstruction"
+# python uniter_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode saturated --evaluation-method noisy_qasm_simulator 2>&1 | tee ./logs/hwea_uniter_logs.txt
+
+# echo "Running sametotal noisy qasm evaluator"
+# mpiexec -n 5 python evaluator_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode sametotal --evaluation-method noisy_qasm_simulator
+# echo "Running reconstruction"
+# python uniter_prob.py --device-name ibmq_johannesburg --circuit-type hwea --shots-mode sametotal --evaluation-method noisy_qasm_simulator 2>&1 | tee ./logs/hwea_uniter_logs.txt
+
+# echo "Running evaluator evaluator"
+# mpiexec -n 5 python evaluator_prob.py --device-name ibmq_johannesburg --circuit-type hwea --evaluation-method statevector_simulator
+# echo "Running reconstruction"
+# python uniter_prob.py --device-name ibmq_johannesburg --circuit-type hwea --evaluation-method statevector_simulator 2>&1 | tee ./logs/hwea_uniter_logs.txt
+
+# python plot.py
