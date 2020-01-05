d60f90b9fd21d8208e3a77be7a84e532a57cc75a
weiT1993 <tangwei1027@gmail.com>
2020-01-05 01:12:17 -0500

    finished tensored mitigation, need to implement for variable mitigation patterns

--- tmp_test.py
+++ tmp_test.py
@@ -1,9 +1,11 @@
 from utils.helper_fun import generate_circ
 from utils.mitigation import TensoredMitigation
 
-circ = generate_circ(full_circ_size=8,circuit_type='supremacy')
+circ = generate_circ(full_circ_size=12,circuit_type='supremacy')
 circ_dict = {'test':{'circ':circ}}
 
 mitigation = TensoredMitigation(circ_dict=circ_dict,device_name='ibmq_boeblingen')
 mitigation.run()
 mitigation.retrieve()
+calibration_matrices = mitigation.calibration_matrices
+print(calibration_matrices['test'])
--- utils/mitigation.py
+++ utils/mitigation.py
@@ -1,3 +1,5 @@
+import numpy as np
+from time import time
 from qiskit.circuit.quantumregister import QuantumRegister
 from utils.helper_fun import get_evaluator_info
 from qiskit.ignis.mitigation.measurement import tensored_meas_cal
@@ -37,10 +39,10 @@ class TensoredMitigation:
             for q in _initial_layout:
                 if 'ancilla' not in _initial_layout[q].register.name:
                     mit_pattern.append([q])
-            meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, circlabel='mcal')
+            meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, circlabel='')
             meas_calibs_transpiled = transpile(meas_calibs, backend=evaluator_info['device'])
-            for circ in meas_calibs_transpiled:
-                meas_calibs_dict.update({'%s|%s'%(key,circ.name):{'circ':circ,'shots':device_max_shots}})
+            for meas_calib_circ in meas_calibs_transpiled:
+                meas_calibs_dict.update({'%s|%s'%(key,meas_calib_circ.name.split('_')[1]):{'circ':meas_calib_circ,'shots':device_max_shots}})
         return meas_calibs_dict
 
     def run(self,real_device=False):
@@ -48,3 +50,45 @@ class TensoredMitigation:
 
     def retrieve(self):
         self.scheduler.retrieve()
+        self.calibration_matrices = {}
+        for key in self.circ_dict:
+            circ = self.circ_dict[key]['circ']
+            num_qubits = len(circ.qubits)
+            perturbation_probabilities = [[0,0,0,0] for i in range(num_qubits)]
+            meas_calib_0_key = '%s|%s'%(key,'0'*num_qubits)
+            meas_calib_0_prob = self.scheduler.circ_dict[meas_calib_0_key]['hw']
+            for state, prob in enumerate(meas_calib_0_prob):
+                bin_state = bin(state)[2:].zfill(num_qubits)
+                for qubit_idx, b in enumerate(bin_state):
+                    b = int(b)
+                    perturbation_probabilities[qubit_idx][b] += prob
+            
+            meas_calib_1_key = '%s|%s'%(key,'1'*num_qubits)
+            meas_calib_1_prob = self.scheduler.circ_dict[meas_calib_1_key]['hw']
+            for state, prob in enumerate(meas_calib_1_prob):
+                bin_state = bin(state)[2:].zfill(num_qubits)
+                for qubit_idx, b in enumerate(bin_state):
+                    b = int(b)
+                    perturbation_probabilities[qubit_idx][b+2] += prob
+
+            # print('Key %s has perturbation_probabilities:'%key,perturbation_probabilities)
+            self.calibration_matrices[key] = self.get_calibration_matrix(perturbation_probabilities=perturbation_probabilities)
+    
+    def get_calibration_matrix(self,perturbation_probabilities):
+        num_qubits = len(perturbation_probabilities)
+        begin = time()
+        base = [1]
+        calibration_matrix = np.zeros(shape=(2**num_qubits,2**num_qubits))
+        for qubit_perturbation in perturbation_probabilities:
+            base = np.kron(base,qubit_perturbation)
+        for meas in range(2**num_qubits):
+            meas_state = bin(meas)[2:].zfill(num_qubits)
+            for actual in range(2**num_qubits):
+                actual_state = bin(actual)[2:].zfill(num_qubits)
+                binary_position_str = ''
+                for a,m in zip(actual_state,meas_state):
+                    binary_position_str += '%s%s'%(a,m)
+                position = int(binary_position_str,2)
+                calibration_matrix[meas][actual] = base[position]
+        print(time()-begin)
+        return calibration_matrix
