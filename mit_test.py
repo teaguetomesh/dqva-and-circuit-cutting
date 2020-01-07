e7e35b43c2d02c4c892193c3cb228110e79466bd
weiT1993 <tangwei1027@gmail.com>
2020-01-07 18:00:54 -0500

    added mitigation.apply

--- mit_test.py
+++ mit_test.py
@@ -5,6 +5,7 @@ from qiskit.converters import circuit_to_dag, dag_to_circuit
 from qiskit.extensions.standard import HGate, SGate, SdgGate, XGate, RXGate, RYGate, RZGate
 from utils.helper_fun import generate_circ, get_evaluator_info, reverseBits
 from utils.mitigation import TensoredMitigation
+from utils.submission import Scheduler
 import copy
 import math
 
@@ -15,22 +16,36 @@ evaluator_info = get_evaluator_info(circ=circ,device_name=device_name,
         fields=['device','basis_gates','coupling_map','properties','initial_layout','noise_model'])
 device_max_experiments = evaluator_info['device'].configuration().max_experiments
 device_max_shots = evaluator_info['device'].configuration().max_shots
+circ_dict = {'test':{'circ':circ,'shots':device_max_shots}}
 
-qr = QuantumRegister(len(evaluator_info['properties'].qubits))
+tensored_mitigation = TensoredMitigation(circ_dict=circ_dict,device_name=device_name)
+tensored_mitigation.run()
 
-mit_pattern = []
-qubit_group = []
-_initial_layout = evaluator_info['initial_layout'].get_physical_bits()
-for q in _initial_layout:
-    if 'ancilla' not in _initial_layout[q].register.name and 2**(len(qubit_group)+1)<=device_max_experiments:
-        qubit_group.append(q)
-    else:
-        mit_pattern.append(qubit_group)
-        qubit_group = [q]
-if len(qubit_group)>0:
-    mit_pattern.append(qubit_group)
+scheduler = Scheduler(circ_dict=circ_dict,device_name=device_name)
+scheduler.run()
 
-meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, circlabel='')
+scheduler.retrieve()
+tensored_mitigation.retrieve()
+tensored_mitigation.apply(unmitigated=scheduler.circ_dict)
+mitigated_circ_dict = tensored_mitigation.circ_dict
+print(mitigated_circ_dict['test'].keys())
+
+
+# qr = QuantumRegister(len(evaluator_info['properties'].qubits))
+
+# mit_pattern = []
+# qubit_group = []
+# _initial_layout = evaluator_info['initial_layout'].get_physical_bits()
+# for q in _initial_layout:
+#     if 'ancilla' not in _initial_layout[q].register.name and 2**(len(qubit_group)+1)<=device_max_experiments:
+#         qubit_group.append(q)
+#     else:
+#         mit_pattern.append(qubit_group)
+#         qubit_group = [q]
+# if len(qubit_group)>0:
+#     mit_pattern.append(qubit_group)
+
+# meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, circlabel='')
 
 # modified_circ = meas_calibs[0]
 # modified_dag = circuit_to_dag(modified_circ)
@@ -38,24 +53,17 @@ meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, ci
 # modified_circ = dag_to_circuit(modified_dag)
 # meas_calibs[0] = modified_circ
 
-meas_calibs_transpiled = transpile(meas_calibs, backend=evaluator_info['device'])
-qobj = assemble(meas_calibs_transpiled, backend=evaluator_info['device'], shots=device_max_shots)
+# meas_calibs_transpiled = transpile(meas_calibs, backend=evaluator_info['device'])
+# qobj = assemble(meas_calibs_transpiled, backend=evaluator_info['device'], shots=device_max_shots)
 # print('Qiskit tensored mitigation mit_pattern:',mit_pattern)
 
-backend = Aer.get_backend('qasm_simulator')
-job = Aer.get_backend('qasm_simulator').run(qobj)
-cal_results = job.result()
-meas_fitter = TensoredMeasFitter(cal_results, mit_pattern=mit_pattern)
-calibration_matrix = copy.deepcopy(meas_fitter.cal_matrices[0])
-for row_idx in range(2**full_circ_size):
-    reversed_row_idx = reverseBits(num=row_idx,bitSize=full_circ_size)
-    for col_idx in range(2**full_circ_size):
-        reversed_col_idx = reverseBits(num=col_idx,bitSize=full_circ_size)
-        calibration_matrix[reversed_row_idx][reversed_col_idx] = meas_fitter.cal_matrices[0][row_idx][col_idx]
-
-circ_dict = {'test':{'circ':circ}}
-tensored_mitigation = TensoredMitigation(circ_dict=circ_dict,device_name=device_name)
-tensored_mitigation.run()
-tensored_mitigation.retrieve()
-circ_dict = tensored_mitigation.circ_dict
-calibration_matrix = circ_dict['test']['calibration_matrix']
+# backend = Aer.get_backend('qasm_simulator')
+# job = Aer.get_backend('qasm_simulator').run(qobj)
+# cal_results = job.result()
+# meas_fitter = TensoredMeasFitter(cal_results, mit_pattern=mit_pattern)
+# calibration_matrix = copy.deepcopy(meas_fitter.cal_matrices[0])
+# for row_idx in range(2**full_circ_size):
+#     reversed_row_idx = reverseBits(num=row_idx,bitSize=full_circ_size)
+#     for col_idx in range(2**full_circ_size):
+#         reversed_col_idx = reverseBits(num=col_idx,bitSize=full_circ_size)
+#         calibration_matrix[reversed_row_idx][reversed_col_idx] = meas_fitter.cal_matrices[0][row_idx][col_idx]
--- utils/mitigation.py
+++ utils/mitigation.py
@@ -124,90 +124,12 @@ class TensoredMitigation:
         print('Computing calibration matrix for %d qubit circuit took %.3e seconds'%(num_qubits,time()-begin))
         return calibration_matrix
 
-class LocalMitigation:
-    def __init__(self,circ_dict,device_name):
-        self.circ_dict = circ_dict
-        self.device_name = device_name
-        self.check_status()
-        self.meas_calibs_dict = self.get_mitigation_circuits()
-        self.scheduler = Scheduler(circ_dict=self.meas_calibs_dict,device_name=self.device_name)
-    
-    def check_status(self):
-        assert isinstance(self.circ_dict,dict)
-        for key in self.circ_dict:
-            value = self.circ_dict[key]
-            if 'circ' not in value:
-                raise Exception('Input circ_dict does not have circ for key {}'.format(key))
-        try:
-            evaluator_info = get_evaluator_info(circ=None,device_name=self.device_name,fields=['device','properties'])
-        except:
-            raise Exception('Illegal input device : {}'.format(self.device_name))
-    
-    def get_mitigation_circuits(self):
-        meas_calibs_dict = {}
-        for key in self.circ_dict:
-            circ = self.circ_dict[key]['circ']
-            evaluator_info = get_evaluator_info(circ=circ,device_name=self.device_name,
-            fields=['device','basis_gates','coupling_map','properties','initial_layout'])
-            device_max_shots = evaluator_info['device'].configuration().max_shots
-            num_qubits = len(evaluator_info['properties'].qubits)
-            qr = QuantumRegister(num_qubits)
-            mit_pattern = []
-            _initial_layout = evaluator_info['initial_layout'].get_physical_bits()
-            for q in _initial_layout:
-                if 'ancilla' not in _initial_layout[q].register.name:
-                    mit_pattern.append([q])
-            meas_calibs, state_labels = tensored_meas_cal(mit_pattern=mit_pattern, qr=qr, circlabel='')
-            meas_calibs_transpiled = transpile(meas_calibs, backend=evaluator_info['device'])
-            for meas_calib_circ in meas_calibs_transpiled:
-                meas_calibs_dict_key = '%s|%s'%(key,meas_calib_circ.name.split('_')[1])
-                assert meas_calibs_dict_key not in meas_calibs_dict
-                meas_calibs_dict.update({meas_calibs_dict_key:{'circ':meas_calib_circ,'shots':device_max_shots}})
-        return meas_calibs_dict
-
-    def run(self,real_device=False):
-        self.scheduler.run(real_device=real_device)
-
-    def retrieve(self):
-        self.scheduler.retrieve()
-        for key in self.circ_dict:
-            circ = self.circ_dict[key]['circ']
-            num_qubits = len(circ.qubits)
-            perturbation_probabilities = [[0,0,0,0] for i in range(num_qubits)]
-            meas_calib_0_key = '%s|%s'%(key,'0'*num_qubits)
-            meas_calib_0_prob = self.scheduler.circ_dict[meas_calib_0_key]['hw']
-            for state, prob in enumerate(meas_calib_0_prob):
-                bin_state = bin(state)[2:].zfill(num_qubits)
-                for qubit_idx, b in enumerate(bin_state):
-                    b = int(b)
-                    perturbation_probabilities[qubit_idx][b] += prob
-            
-            meas_calib_1_key = '%s|%s'%(key,'1'*num_qubits)
-            meas_calib_1_prob = self.scheduler.circ_dict[meas_calib_1_key]['hw']
-            for state, prob in enumerate(meas_calib_1_prob):
-                bin_state = bin(state)[2:].zfill(num_qubits)
-                for qubit_idx, b in enumerate(bin_state):
-                    b = int(b)
-                    perturbation_probabilities[qubit_idx][b+2] += prob
-
-            # print('Key %s has perturbation_probabilities:'%key,perturbation_probabilities)
-            self.circ_dict[key]['calibration_matrix'] = self.get_calibration_matrix(perturbation_probabilities=perturbation_probabilities)
-    
-    def get_calibration_matrix(self,perturbation_probabilities):
-        num_qubits = len(perturbation_probabilities)
-        begin = time()
-        base = [1]
-        calibration_matrix = np.zeros(shape=(2**num_qubits,2**num_qubits))
-        for qubit_perturbation in perturbation_probabilities:
-            base = np.kron(base,qubit_perturbation)
-        for meas in range(2**num_qubits):
-            meas_state = bin(meas)[2:].zfill(num_qubits)
-            for actual in range(2**num_qubits):
-                actual_state = bin(actual)[2:].zfill(num_qubits)
-                binary_position_str = ''
-                for a,m in zip(actual_state,meas_state):
-                    binary_position_str += '%s%s'%(a,m)
-                position = int(binary_position_str,2)
-                calibration_matrix[meas][actual] = base[position]
-        print(time()-begin)
-        return calibration_matrix
+    def apply(self,unmitigated):
+        for key in unmitigated:
+            assert key in self.circ_dict
+            calibration_matrix = self.circ_dict[key]['calibration_matrix']
+            filter_matrix = np.linalg.inv(calibration_matrix)
+            unmitigated_prob = np.reshape(unmitigated[key]['hw'],(-1,1))
+            mitigated_prob = np.reshape(filter_matrix.dot(unmitigated_prob),(1,-1)).tolist()[0]
+            assert abs(sum(mitigated_prob)-1)<1e-10
+            self.circ_dict[key]['mitigated_hw'] = mitigated_prob
