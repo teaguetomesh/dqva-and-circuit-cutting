b1c3f611c0520f7485c3a5e2901388b6bb790b88
weiT1993 <tangwei1027@gmail.com>
2020-01-09 00:24:20 -0500

    testing stack

--- hardware_run.sh
+++ hardware_run.sh
@@ -5,7 +5,7 @@ if [ ! -d "./hardware/logs/" ]; then
   mkdir ./hardware/logs/
 fi
 
-python -m hardware.generator --min-qubit 2 --max-qubit 9 --max-clusters 5 --device-name $DEVICE_NAME --circuit-type $CIRCUIT_TYPE 2>&1 | tee -a ./hardware/logs/$CIRCUIT_TYPE\_$DEVICE_NAME\_logs.txt
+python -m hardware.generator --min-qubit 2 --max-qubit 10 --max-clusters 3 --device-name $DEVICE_NAME --circuit-type $CIRCUIT_TYPE 2>&1 | tee -a ./hardware/logs/$CIRCUIT_TYPE\_$DEVICE_NAME\_logs.txt
 
 mpiexec -n 2 python -m utils.evaluator --experiment-name hardware --device-name $DEVICE_NAME --circuit-type $CIRCUIT_TYPE --shots-mode saturated --evaluation-method hardware
 python -m hardware.job_submittor --device-name $DEVICE_NAME --circuit-type $CIRCUIT_TYPE --shots-mode saturated 2>&1 | tee -a ./hardware/logs/$CIRCUIT_TYPE\_$DEVICE_NAME\_logs.txt
