c29e6941fddd5b4895558a09f8fe30b003235c84
weiT1993 <tangwei1027@gmail.com>
2020-01-20 13:04:19 -0500

    testing with chi2 metric

--- mit_test.py
+++ mit_test.py
@@ -7,6 +7,7 @@ from qiskit.tools.visualization import plot_histogram
 from qiskit.providers.aer import noise
 from utils.helper_fun import generate_circ, get_evaluator_info, apply_measurement, evaluate_circ
 from utils.metrics import cross_entropy
+from scipy.stats import chisquare
 from utils.mitigation import TensoredMitigation
 from utils.submission import Scheduler
 from utils.conversions import list_to_dict, dict_to_array
@@ -14,10 +15,30 @@ import copy
 import math
 import numpy as np
 
+def compute_metrics(ground_truth,raw_counts,mitigated_counts,metric):
+    if metric == 'ce':
+        truth_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        obs=dict_to_array(distribution_dict=ground_truth,force_prob=True))
+        raw_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        obs=dict_to_array(distribution_dict=raw_counts,force_prob=True))
+        mit_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        obs=dict_to_array(distribution_dict=mitigated_counts,force_prob=True))
+        return truth_ce, raw_ce, mit_ce
+    elif metric == 'chi2':
+        truth_chi2 = chisquare(f_exp=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        f_obs=dict_to_array(distribution_dict=ground_truth,force_prob=True))[0]
+        raw_chi2 = chisquare(f_exp=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        f_obs=dict_to_array(distribution_dict=raw_counts,force_prob=True))[0]
+        mit_chi2 = chisquare(f_exp=dict_to_array(distribution_dict=ground_truth,force_prob=True),
+        f_obs=dict_to_array(distribution_dict=mitigated_counts,force_prob=True))[0]
+        return truth_chi2, raw_chi2, mit_chi2
+    else:
+        raise Exception('gg')
+
 device_name = 'ibmq_boeblingen'
 evaluator_info = get_evaluator_info(circ=None,device_name='ibmq_boeblingen',fields=['basis_gates','properties'])
 device_qubits = len(evaluator_info['properties'].qubits)
-# Make a 3Q GHZ state
+
 full_circ_size = 7
 qr = QuantumRegister(full_circ_size)
 ghz = generate_circ(full_circ_size=full_circ_size,circuit_type='supremacy')
@@ -76,9 +97,9 @@ scheduler.run(real_device=False)
 
 scheduler.retrieve(force_prob=False)
 tensored_mitigation.retrieve()
-# np.set_printoptions(formatter={'all':lambda x: '%.3f'%x})
-# for qiskit_cal_mat, my_cal_mat in zip(meas_fitter.cal_matrices, tensored_mitigation.circ_dict['test']['calibration_matrices']):
-#     print(qiskit_cal_mat-my_cal_mat)
+np.set_printoptions(formatter={'all':lambda x: '%.2f'%x})
+for qiskit_cal_mat, my_cal_mat in zip(meas_fitter.cal_matrices, tensored_mitigation.circ_dict['test']['calibration_matrices']):
+    print(qiskit_cal_mat-my_cal_mat)
 tensored_mitigation.apply(unmitigated=scheduler.circ_dict,force_prob=False)
 mitigated_circ_dict = tensored_mitigation.circ_dict
 print(mitigated_circ_dict['test'].keys())
@@ -87,23 +108,17 @@ my_mitigated = mitigated_circ_dict['test']['mitigated_hw']
 my_raw_dict = list_to_dict(l=list(my_raw))
 my_mitigated_dict = list_to_dict(l=list(my_mitigated))
 
-fig = plot_histogram([raw_counts, mitigated_counts, ground_truth], legend=['raw','mitigated','truth'],figsize=(20,10),title='qiskit mitigation')
+truth_ce, qiskit_raw_ce, qiskit_mit_ce = compute_metrics(ground_truth=ground_truth,raw_counts=raw_counts,mitigated_counts=mitigated_counts,metric='chi2')
+print('Qiskit \u0394H: {:.3e}-->{:.3e}'.format(qiskit_raw_ce,qiskit_mit_ce))
+
+fig = plot_histogram([raw_counts, mitigated_counts, ground_truth], legend=['raw = %.3e'%qiskit_raw_ce,'mitigated = %.3e'%qiskit_mit_ce,'truth = %.3e'%truth_ce],figsize=(35,10),title='qiskit mitigation')
 fig.savefig('qiskit_mitigation.png')
 
-fig = plot_histogram([my_raw_dict, my_mitigated_dict,ground_truth], legend=['my_raw', 'my_mitigated','truth'],figsize=(20,10),title='my mitigation')
+truth_ce, my_raw_ce, my_mit_ce = compute_metrics(ground_truth=ground_truth,raw_counts=my_raw_dict,mitigated_counts=my_mitigated_dict,metric='chi2')
+print('My \u0394H: {:.3e}-->{:.3e}'.format(my_raw_ce,my_mit_ce))
+
+fig = plot_histogram([my_raw_dict, my_mitigated_dict,ground_truth], legend=['my_raw = %.3e'%my_raw_ce,'my_mitigated = %.3e'%my_mit_ce,'truth = %.3e'%truth_ce],figsize=(35,10),title='my mitigation')
 fig.savefig('my_mitigation.png')
 
-fig = plot_histogram([mitigated_counts, my_mitigated_dict], legend=['mitigated', 'my_mitigated'],figsize=(20,10),title='mitigations comparison')
+fig = plot_histogram([mitigated_counts, my_mitigated_dict, ground_truth], legend=['mitigated', 'my_mitigated', 'truth'],figsize=(35,10),title='mitigations comparison')
 fig.savefig('mitigations.png')
-
-qiskit_raw_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
-obs=dict_to_array(distribution_dict=raw_counts,force_prob=True))
-qiskit_mit_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
-obs=dict_to_array(distribution_dict=mitigated_counts,force_prob=True))
-
-my_raw_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
-obs=dict_to_array(distribution_dict=my_raw_dict,force_prob=True))
-my_mit_ce = cross_entropy(target=dict_to_array(distribution_dict=ground_truth,force_prob=True),
-obs=dict_to_array(distribution_dict=my_mitigated_dict,force_prob=True))
-
-print('Qiskit : {:.3e}-->{:.3e}, My : {:.3e}-->{:.3e}'.format(qiskit_raw_ce,qiskit_mit_ce,my_raw_ce,my_mit_ce))
