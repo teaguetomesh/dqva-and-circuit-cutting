6346049821635cca15ee38c38ee74e212e8fd444
weiT1993 <tangwei1027@gmail.com>
2020-02-04 09:47:41 -0500

    checking output

--- utils/check_output.py
+++ utils/check_output.py
@@ -18,28 +18,28 @@ if __name__ == '__main__':
     plotter_input = read_file(dirname+plotter_input_filename)
     print(plotter_input.keys())
 
-    case = (5,12)
+    case = (5,7)
     plotter_input = plotter_input[case]
 
-    d1 = plotter_input['fc_evaluations']['sv_noiseless']
-    d2 = plotter_input['fc_evaluations']['qasm']
+    d1 = plotter_input['sv']
+    d2 = plotter_input['qasm']
     if args.experiment_name == 'simulator':
-        d3 = plotter_input['fc_evaluations']['qasm+noise']
+        d3 = plotter_input['qasm+noise']
     else:
-        d3 = plotter_input['fc_evaluations']['hw']
+        d3 = plotter_input['hw']
     d4 = [abs(x) for x in plotter_input['cutting']]
     
-    ground_truth_ce = cross_entropy(target=d1,obs=d1)
-    cutting_ce = cross_entropy(target=d1,obs=d4)
-    std_ce = cross_entropy(target=d1,obs=d3)
-    ce_percent_change = 100*(std_ce - cutting_ce)/(std_ce - ground_truth_ce)
+    ground_truth_metric = chi2_distance(target=d1,obs=d1)
+    cutting_metric = chi2_distance(target=d1,obs=d4)
+    std_metric = chi2_distance(target=d1,obs=d3)
+    metric_percent_change = 100*(std_metric - cutting_metric)/(std_metric - ground_truth_metric)
 
     ground_truth_fid = fidelity(target=d1,obs=d1)
     cutting_fid = fidelity(target=d1,obs=d4)
     std_fid = fidelity(target=d1,obs=d3)
     fid_percent_change = 100*(cutting_fid-std_fid)/std_fid
     print('std fid = %.3f, cutting fid = %.3f'%(std_fid,cutting_fid))
-    print('ce reduction =',ce_percent_change,'fid improvement =',fid_percent_change)
+    print('metric reduction =',metric_percent_change,'fid improvement =',fid_percent_change)
 
     plot_range = min(1024,len(d1))
     x = np.arange(len(d1))[:plot_range]
@@ -50,7 +50,7 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(10,5))
     plt.subplot(221)
-    plt.bar(x,height=d1[:plot_range],label='ground truth, fid = %.3e, \u0394H = %.3e'%(fidelity(d1,d1),cross_entropy(d1,d1)-cross_entropy(d1,d1)))
+    plt.bar(x,height=d1[:plot_range],label='ground truth, fid = %.3e, metric = %.3e'%(fidelity(d1,d1),chi2_distance(d1,d1)))
     plt.ylim(0,y_lim)
     plt.xlabel('quantum state')
     plt.ylabel('probability')
@@ -58,21 +58,21 @@ if __name__ == '__main__':
 
 
     plt.subplot(222)
-    plt.bar(x,height=d4[:plot_range],label='cutting mode, fid = %.3e, \u0394H = %.3e'%(fidelity(d1,d4),cross_entropy(d1,d4)-cross_entropy(d1,d1)))
+    plt.bar(x,height=d4[:plot_range],label='cutting mode, fid = %.3e, metric = %.3e'%(fidelity(d1,d4),chi2_distance(d1,d4)))
     plt.ylim(0,y_lim)
     plt.xlabel('quantum state')
     plt.ylabel('probability')
     plt.legend()
 
     plt.subplot(223)
-    plt.bar(x,height=d3[:plot_range],label='standard mode, fid = %.3e, \u0394H = %.3e'%(fidelity(d1,d3),cross_entropy(d1,d3)-cross_entropy(d1,d1)))
+    plt.bar(x,height=d3[:plot_range],label='standard mode, fid = %.3e, metric = %.3e'%(fidelity(d1,d3),chi2_distance(d1,d3)))
     plt.ylim(0,y_lim)
     plt.xlabel('quantum state')
     plt.ylabel('probability')
     plt.legend()
 
     plt.subplot(224)
-    plt.bar(x,height=d2[:plot_range],label='noiseless qasm, fid = %.3e, \u0394H = %.3e'%(fidelity(d1,d2),cross_entropy(d1,d2)-cross_entropy(d1,d1)))
+    plt.bar(x,height=d2[:plot_range],label='noiseless qasm, fid = %.3e, metric = %.3e'%(fidelity(d1,d2),chi2_distance(d1,d2)))
     plt.ylim(0,y_lim)
     plt.xlabel('quantum state')
     plt.ylabel('probability')
