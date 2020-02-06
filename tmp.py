6ace9f1fe31a46f6873ef7a9083b79d7a1caf42a
weiT1993 <tangwei1027@gmail.com>
2020-02-06 01:07:05 -0500

    debugging searcher

--- tmp.py
+++ tmp.py
@@ -1,42 +1,19 @@
 from utils.helper_fun import generate_circ, get_evaluator_info, evaluate_circ, apply_measurement
-from utils.metrics import fidelity
-from utils.conversions import dict_to_array
-from utils.schedule import Scheduler
-from qiskit import execute, Aer
+import utils.MIQCP_searcher as searcher
+import utils.cutter as cutter
+from time import time
 
+fc_size = 40
+circ = generate_circ(full_circ_size=fc_size,circuit_type='supremacy')
+max_clusters = 30
+cluster_max_qubit = 35
+searcher_begin = time()
+hardness, positions, ancilla, d, num_cluster, m = searcher.find_cuts(circ=circ,reconstructor_runtime_params=[4.275e-9,6.863e-1],reconstructor_weight=0,
+num_clusters=range(2,min(len(circ.qubits),max_clusters)+1),cluster_max_qubit=cluster_max_qubit)
+searcher_time = time() - searcher_begin
+print('searcher time = ',searcher_time)
 
-circ = generate_circ(full_circ_size=20,circuit_type='bv')
-num_shots = 8192*500
-evaluator_info = get_evaluator_info(circ=circ,device_name='ibmq_boeblingen',fields=['device','basis_gates',
-    'coupling_map','properties','initial_layout','noise_model'])
-evaluator_info['num_shots'] = num_shots
-print(evaluator_info['initial_layout'])
-
-ground_truth = evaluate_circ(circ=circ,backend='statevector_simulator',evaluator_info=None,force_prob=True)
-ground_truth = dict_to_array(distribution_dict=ground_truth,force_prob=True)
-# noisy = evaluate_circ(circ=circ,backend='noisy_qasm_simulator',evaluator_info=evaluator_info,force_prob=True)
-# noisy = dict_to_array(distribution_dict=noisy,force_prob=True)
-# print('noisy sim fidelity = ',fidelity(target=ground_truth,obs=noisy))
-
-circ_dict = {}
-circ_dict['test'] = {'circ':circ,'shots':num_shots,'initial_layout':evaluator_info['initial_layout']}
-scheduler = Scheduler(circ_dict=circ_dict,device_name='ibmq_boeblingen')
-scheduler.run(real_device=True)
-scheduler.retrieve(force_prob=False)
-circ_dict = scheduler.circ_dict
-print('my hw fidelity = ', fidelity(target=ground_truth,obs=circ_dict['test']['hw']))
-
-# qc = apply_measurement(circ)
-# result = execute([qc for x in range(10)],backend=Aer.get_backend('qasm_simulator'),shots=8192).result()
-# result = execute([qc for x in range(10)],backend=evaluator_info['device'],initial_layout=evaluator_info['initial_layout'],shots=8192).result()
-# all_counts = {}
-# for i in range(10):
-#     counts = result.get_counts(i)
-#     for state in counts:
-#         if state in all_counts:
-#             all_counts[state] += counts[state]
-#         else:
-#             all_counts[state] = counts[state]
-# print(sum(all_counts.values()))
-# qiskit_prob = dict_to_array(all_counts,force_prob=True)
-# print('qiskit hw fidelity = ', fidelity(target=ground_truth,obs=qiskit_prob))
+if m != None:
+    m.print_stat()
+else:
+    print('NOT feasible')
