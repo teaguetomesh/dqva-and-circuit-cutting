8b02dbfcff8f89ef66bdbaa364937112f696a282
weiT1993 <tangwei1027@gmail.com>
2020-02-17 10:33:46 -0500

    update

--- make_revision_plot.py
+++ make_revision_plot.py
@@ -13,12 +13,12 @@ ind = np.arange(len(compute_time))    # the x locations for the groups
 width = 0.35       # the width of the bars: can also be len(x) sequence
 
 p1 = plt.bar(ind, compute_time, width)
-p2 = plt.bar(ind, reorder_time, width,bottom=compute_time)
+# p2 = plt.bar(ind, reorder_time, width,bottom=compute_time)
 
 plt.ylabel('Time (s)')
 plt.title('20-qubit BV Circuit Cut into Four Clusters')
 plt.xticks(ind, ('Single thread', 'Smart order', '4-Thread Parallel'))
 # plt.yticks(np.arange(0, 81, 10))
-plt.legend((p1[0], p2[0]), ('Compute', 'Reorder'))
+# plt.legend((p1[0]), ['Compute'])
 
 plt.savefig('./large_on_small/techniques_demonstration.png',dpi=400)
