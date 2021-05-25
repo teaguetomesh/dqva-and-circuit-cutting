124226adc239a18093675acd868c662e30253453
Teague Tomesh <ttomesh@princeton.edu>
2021-05-25 14:09:31 -0400

    script for running many jobs

--- run_dqva_and_cutting.py
+++ run_dqva_and_cutting.py
@@ -1,36 +1,68 @@
-import glob
+
+import sys, os, argparse, glob
 import dqva
 import partition_no_cuts
 import pickle
 import networkx as nx
 from utils.graph_funcs import graph_from_file
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', type=str, default=None,
+                        help='path to dqva project')
+    parser.add_argument('--graph', type=str, default=None,
+                        help='glob path to the benchmark graph(s)')
+    parser.add_argument('--numcuts', type=int, default=1,
+                        help='Number of cuts')
+    parser.add_argument('--rounds', type=int, default=4,
+                         help='Number of partition rounds')
+    parser.add_argument('--shots', type=int, default=8192,
+                        help='Number of shots')
+    args = parser.parse_args()
+    return args
 
-cutting = False
 
-#all_graphs = glob.glob("benchmark_graphs/N12_p20_graphs/*")
-gfile = 'benchmark_graphs/N26_3regular_cutting_graph.txt'
-G = graph_from_file(gfile)
-#gfile = '3-regular'
-#G = nx.generators.random_graphs.random_regular_graph(3, 26)
-print('Generated graph:', gfile, 'with {} nodes'.format(G.number_of_nodes()))
+def main():
+    args = parse_args()
 
-init_state = '0'*G.number_of_nodes()
-full_history = []
-for rounds in range(4):
-    print('-------------- ROUND {} BEGIN --------------\n\n'.format(rounds+1))
-    if cutting:
-        out = dqva.solve_mis_cut_dqva(init_state, G, m=1, verbose=1,
-                                      shots=50000, max_cuts=1)
-    else:
-        out = partition_no_cuts.solve_mis_no_cut_dqva(init_state, G, m=1,
-                                                      shots=20000, verbose=1)
-    init_state = out[0]
-    full_history.append(out)
+    DQVAROOT = args.path
+    if DQVAROOT[-1] != '/':
+        DQVAROOT += '/'
+    sys.path.append(DQVAROOT)
 
-if cutting:
-    savefn = 'dqva_and_cutting_1cuts.pickle'
-else:
-    savefn = 'dqva_and_NO_cutting.pickle'
-with open('benchmark_results/'+savefn, 'wb') as pf:
-    pickle.dump((G, full_history), pf)
+    all_graphs = glob.glob(DQVAROOT + args.graph)
+
+    savepath = DQVAROOT + 'benchmark_results/dqva_{}cuts/'.format(args.numcuts)
+    if not os.path.isdir(savepath):
+        os.mkdir(savepath)
+
+    for graphfn in all_graphs:
+        graphname = graphfn.split('/')[-1].strip('.txt')
+        cur_savepath = savepath + '{}/'.format(graphname)
+        if not os.path.isdir(cur_savepath):
+            os.mkdir(cur_savepath)
+
+        G = graph_from_file(graphfn)
+        nq = len(G.nodes)
+
+        print('Loaded graph: {}, with {} nodes'.format(graphfn, nq))
+
+        init_state = '0'*G.number_of_nodes()
+        full_history = []
+        for rounds in range(args.rounds):
+            print('-------------- ROUND {} BEGIN --------------\n\n'.format(rounds+1))
+            if args.numcuts > 0:
+                out = dqva.solve_mis_cut_dqva(init_state, G, m=1, verbose=1,
+                                        shots=args.shots, max_cuts=args.numcuts)
+            else:
+                out = partition_no_cuts.solve_mis_no_cut_dqva(init_state, G, m=1,
+                                                    shots=args.shots, verbose=1)
+            init_state = out[0]
+            full_history.append(out)
+
+        savefn = 'dqva_{}_{}cuts.pickle'.format(graphname, args.numcuts)
+        with open(cur_savepath+savefn, 'wb') as pf:
+            pickle.dump((G, full_history), pf)
+
+if __name__ == '__main__':
+    main()
