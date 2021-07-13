6e922c147c1fecc31de3d44715d5e8d05902e623
Teague Tomesh <ttomesh@chai.CS.Princeton.EDU>
2021-07-13 15:01:05 -0400

    obtain optimal mis via brute force search

--- brute_force_cutting_graph.py
+++ brute_force_cutting_graph.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import argparse, sys, time
+import argparse, glob, sys, time
 from pathlib import Path
 from utils import helper_funcs, graph_funcs
 
@@ -15,27 +15,29 @@ def parse_args():
 def main():
     args = parse_args()
 
-    graphtype = args.g.split('/')[-2]
-    graphname = args.g.split('/')[-1].strip('.txt')
+    graphfiles = glob.glob(args.g)
+    for gfile in graphfiles:
+        graphtype = gfile.split('/')[-2]
+        graphname = gfile.split('/')[-1].strip('.txt')
 
-    G = graph_funcs.graph_from_file(args.g)
-    print('graphtype: {}, graphname: {}'.format(graphtype, graphname))
-    print('Loaded graph with {} nodes'.format(len(G.nodes)))
+        G = graph_funcs.graph_from_file(gfile)
+        print('graphtype: {}, graphname: {}'.format(graphtype, graphname))
+        print('Loaded graph with {} nodes'.format(len(G.nodes)))
 
-    start = time.time()
-    opt_strs, opt_mis = helper_funcs.brute_force_search(G)
-    end = time.time()
-    print('Finished brute force search in {:.3f} min'.format((end - start) / 60))
+        start = time.time()
+        opt_strs, opt_mis = helper_funcs.brute_force_search(G)
+        end = time.time()
+        print('Finished brute force search in {:.3f} min'.format((end - start) / 60))
 
-    outdir = 'benchmark_graphs/brute_force_outputs/{}/'.format(graphtype)
-    Path(outdir).mkdir(parents=True, exist_ok=True)
-    outfile = outdir + graphname + '_brute_force.out'
-    with open(outfile, 'w') as fn:
-        fn.write('{}, {}\n'.format(graphtype, graphname))
-        fn.write('Optimal MIS is {}\n'.format(opt_mis))
-        fn.write('Optimal MIS:\n')
-        for bitstr in opt_strs:
-            fn.write('\t{}, valid: {}\n'.format(bitstr, graph_funcs.is_indset(bitstr, G)))
+        outdir = 'benchmark_graphs/brute_force_outputs/{}/'.format(graphtype)
+        Path(outdir).mkdir(parents=True, exist_ok=True)
+        outfile = outdir + graphname + '_brute_force.out'
+        with open(outfile, 'w') as fn:
+            fn.write('{}, {}\n'.format(graphtype, graphname))
+            fn.write('Optimal MIS is {}\n'.format(opt_mis))
+            fn.write('Optimal MIS:\n')
+            for bitstr in opt_strs:
+                fn.write('\t{}, valid: {}\n'.format(bitstr, graph_funcs.is_indset(bitstr, G)))
 
 if __name__ == '__main__':
     main()
