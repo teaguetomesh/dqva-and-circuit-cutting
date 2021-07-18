ebbc3dc50d83092a79caa47c1f5ef199a1d67b42
Teague Tomesh <ttomesh@princeton.edu>
2021-07-17 21:14:10 -0500

    Generate more 3-regular graphs

--- benchmark_graphs/gen_d-regular_benchmark_graphs.py
+++ benchmark_graphs/gen_d-regular_benchmark_graphs.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python
 import glob
 import networkx as nx
 
 dirs = []
-for n in range(18, 26, 2):
+for n in range(18, 28, 2):
     temp_dirs = glob.glob('N{}_d3_graphs*'.format(n))
     dirs.extend(temp_dirs)
 print('Dirs:', dirs)
@@ -12,7 +13,7 @@ for folder in dirs:
     d = int(folder.split('_')[1][1:])
     print('Nodes: {}, degree: {}'.format(n, d))
 
-    for j in range(15):
+    for j in range(15, 50):
         G = nx.random_regular_graph(d, n)
         edges = list(G.edges())
 
